16bbf96e785b11fa8b5275ec75c21649a59eb5c4
Rana Sam <ranausama2082001@gmail.com>
2022-01-12 19:12:14 +0500

    assignment 11
    group 3 in react.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
+import './style.css';
+import  aiden from './img/Aiden.png'
+import  ethel from './img/Ethel.png'
+import  fannie from './img/Fannie.png'
+import  burger from './img/Burger.png'
+import  chiml from './img/ChimlaMirch.png'
+import  fri from './img/Fries.png'
+import  garlic from './img/Garlic.png'
+import  leaf from './img/Leaf.png'
+import  mush from './img/Mushroom.png'
 function App() {
   return (
     <div className="App">
     <div className="group2">
         <div className="textGroup2">
-            <h1>Our Chef</h1>
+            <h1 className="textGroup2h1">Our Chef</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
 
         </div>
         <div className="cardGroup2">
             <div className="card1">
-                <img src='./Aiden.png' alt="" />
+                <img src={aiden} alt="" />
                 <span>
                     <h2>Aiden Hunter</h2>
                     <p>Founder</p>
                 </span>
             </div>
             <div className="card2Group2">
-                <img src="./img/Ethel.png" alt=""/>
+                <img src={ethel} alt=""/>
                 <span>
                     <h2>Ethel Ramsey</h2>
                     <p>Co-founder</p>
                 </span>
             </div>
             <div className="card3Group2">
-                <img src="./img/Fannie.png" alt="" />
+                <img src={fannie} alt="" />
                 <span>
                     <h2>Fannie stewart</h2>
                     <p>Co-founder</p>
@@ -36,12 +45,12 @@ function App() {
 
         </div>
         <div className="designGroup2">
-            <img class="designbugr" src="./img/Burger.png" alt=""/>
-            <img class="designchim" src="./img/ChimlaMirch.png" alt=""/>
-            <img class="designfri" src="./img/Fries.png" alt=""/>
-            <img class="designgar" src="./img/Garlic.png" alt=""/>
-            <img class="designlea" src="./img/Leaf.png" alt=""/>
-            <img class="designmush" src="./img/Mushroom.png" alt=""/>
+            <img class="designbugr" src={burger} alt=""/>
+            <img class="designchim" src={chiml} alt=""/>
+            <img class="designfri" src={fri} alt=""/>
+            <img class="designgar" src={garlic} alt=""/>
+            <img class="designlea" src={leaf} alt=""/>
+            <img class="designmush" src={mush} alt=""/>
         </div>
 
     </div>
