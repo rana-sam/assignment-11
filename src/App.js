f842007575aea5598775d81c45c07bef2569b5b7
Rana Sam <ranausama2082001@gmail.com>
2022-01-13 12:30:44 +0500

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
      <Group2 />
-
      </div>
   );
 }
