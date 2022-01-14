8854baaeba050bbcefd70732b80072f80d3c937d
Rana Sam <ranausama2082001@gmail.com>
2022-01-14 21:33:14 +0500

    Group 5

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import Group2 from "./Component/Group1";
+import Group5 from "./Component/Group2";
 function App() {
   return (
     <div className="App">
      <Group2 />
+     <Group5/>
      </div>
   );
 }
---
+++ src/Component/Group2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Group5() {
+    return (
+        <div>
+            <div className="group5">
+        <div className="textgroup5">
+          <h1>Statistics</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
+        </div>
+        <div className="cardgroup5">
+          <div className="card1group5">
+            <h1>7K</h1>
+            <p>CUSTOMER</p>
+          </div>
+          <div className="card1group5">
+            <h1>109</h1>
+            <p>OUTLETS</p>
+          </div>
+          <div className="card1group5">
+            <h1>35</h1>
+            <p>COUNTRY</p>
+          </div>
+        </div>
+      </div>
+
+        </div>
+    )
+}
