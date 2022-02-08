5298670ee1136ec8cde3df4703de01dc47023648
Rana Sam <ranausama2082001@gmail.com>
2022-02-08 22:31:43 +0500

    Update Group3.jsx

--- src/Component/Group3.jsx
+++ src/Component/Group3.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import burger from '../img/Burger Group 3.png'
+import React from "react";
+import burger from "../img/Burger Group 3.png";
 export default function Group3() {
-    return (
-        <div>
-            <div className="group3">
+  return (
+    <div>
+      <div className="group3">
         <div className="row">
-          <div className="col-6" id="row1group3"><img src={burger} alt="" /></div>
+          <div className="col-6" id="row1group3">
+            <img src={burger} alt="" />
+          </div>
           <div className="col-4" id="row1group3h1">
             <h1>Best Burger</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              lacinia odio vitae vestibulum vestibulum.
+            </p>
             <button type="submit">ORDER NOW</button>
           </div>
           <div className="col-2" />
@@ -18,6 +23,6 @@ export default function Group3() {
           <div className="col-6"> </div>
         </div>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
